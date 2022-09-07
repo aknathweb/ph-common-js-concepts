f21f8cda88dd47d8fe8c4618c01f84046d46b94d
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-09-08 01:00:59 +0600

    use == and === use and non-primitive equal comparison not work

--- triple.js
+++ triple.js
@@ -1,16 +1,35 @@
-// always use ===
-// equal comparison doesnt work for non-primitive
+/* Double equal and Triple equal */
 
-const first = [45, 54, 98];
-const second = [45, 54, 98];
-if(first === second){
+/// Double equal (==) check: only value
+//// use type coerction: Type coercion is the automatic or implicit conversion of values from one data type to another
+// For same type: check value
+// For different type:  type casting + type conversion + check value
+const first1 = 2;
+const second1 = "2";
+if(first1 == second1){
     console.log('values are equal')
 }
 else{
     console.log('values are not equal');
 }
 
-// type coercion
-// type conversion
-// type casting
+/// Triple equal (===) check: value + type
+const first2 = 2;
+const second2 = 2;
+if(first2 == second2){
+    console.log('values are equal')
+}
+else{
+    console.log('values are not equal');
+}
+
+/// equal comparison doesnt work for non-primitive
+const first3 = [45, 54, 98];
+const second3 = [45, 54, 98];
+if(first3 === second3){
+    console.log('values are equal')
+}
+else{
+    console.log('values are not equal');
+}
 
