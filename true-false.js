0bf28dd95c6bba4a2e173e909deb664447aba360
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-09-08 00:34:20 +0600

    true and false check by value and ! sign use

--- true-false.js
+++ true-false.js
@@ -1,20 +1,21 @@
-/* 
-Truthy:
-1. true
-2. any number (+ve, -ve) will be truthy other than 0
-3. any string other than empty string
-4. '0', 'false'
-5. {}
-6. []
+/* Return Ture or False */
+
+/// Truthy: return ture case
+// 1. true
+// 2. any number (+ve, -ve) will be truthy other than 0
+// 3. any string other than empty string
+// 4. '0', 'false'
+// 5. {}
+// 6. []
 
 
-Falsy:
-1. false
-2. 0
-3. '' (empty string)
-4. undefined
-5. null
-*/
+/// Falsy: return false case
+// 1. false
+// 2. 0
+// 3. '' (empty string)
+// 4. undefined
+// 5. null
+
 let x = [5, 6];
 console.log(x);
 if(x){
@@ -24,14 +25,16 @@ else{
     console.log('value of x is falsy');
 }
 
-// optional 
-// check falsy
+/* ! sign use */
+
+/// ! true = false
 const y = null;
 if (!y){
     console.log('value is falsy')
 }
 const z = {class: 9};
-// check true
+
+/// !! true = true
 if(!!x){
     console.log('value is truthy');
 }
