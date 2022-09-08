0b1beaa876394b7722873a8c211d935c3dec759e
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-09-08 10:42:43 +0600

    Block Scope concept

--- scope.js
+++ scope.js
@@ -1,15 +1,26 @@
+/* Block Scope */
+
+///function block scope start
 function add(a, b){
     const total = a+b;
+    ///if block scope start
     if(b>5){
         const sum = 25 + a +b;
     }
+    ///if block scope end
+    ///else block scope end
     else{
         const sum = 5 + a + b;
         var current = sum;
     }
+    ///else block scope end
     console.log(current);
     return total;
 }
+///function block scope end
+
+
 // console.log(a,b);
 // console.log(total);
+
 add(5, 3);
