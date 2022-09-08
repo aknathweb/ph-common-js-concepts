b8d6e5a871b3f0e583405052418f25ebdc6af367
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-09-08 17:34:12 +0600

    arguments = function all parameters value

--- arguments.js
+++ arguments.js
@@ -1,11 +1,22 @@
-// array like object
+/* arguments = function all parameters value */
+
+////arguments are array like object
 function sum(a, b, c){
+
+    //// show function all parameters value = arguments
+    console.log(arguments);
+    //// arguments to args array convert
     const args = [...arguments];
-    // console.log(args);
+    console.log(args);
+    
     const result = a + b + c;
     return result;
 }
-// console.log(arguments);
+//// arguments not work out of fuction
+console.log(arguments);
+
 const total = sum(45, 89, 12, 45, 98, 56);
-// console.log(total);
+console.log(total);
+
+//// show the function arguments length
 console.log(sum.length);
