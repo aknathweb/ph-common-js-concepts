3b4949104d008088e15de331bd2ca29966e78998
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-09-08 11:58:14 +0600

    Closure Concept , function bundle

--- closure.js
+++ closure.js
@@ -1,3 +1,12 @@
+/* Closure Concept */
+
+/// A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function.
+
+/// short: 
+///1. combination of a function
+///2. inner function have access to use outer function data
+
+
 function kitchen(){
     let roast = 0;
     return function(){
