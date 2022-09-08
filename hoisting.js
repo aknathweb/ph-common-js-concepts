f0f6b41055402917bd24a98172b128bf721c3441
Anik Kumar Nath (Web) <108430535+aknathweb@users.noreply.github.com>
2022-09-08 11:03:11 +0600

    how  Hoisting work

--- hoisting.js
+++ hoisting.js
@@ -1,14 +1,24 @@
-// print5();
+/* About Hoisting */
+
+/// Hoisting is the default behavior of moving all the declarations at the top of the scope before code execution
+/// Hoisting work: function declaration and var variable
+/// Hoisting not work: function expression
+
+
+print5();
 console.log(print10);
 print10();
-for(let i = 0; i< 5; i++){
-    // console.log(i);
+for(var i = 0; i< 5; i++){
+    console.log(i);
 }
-// console.log('outside', i);
+console.log('outside', i);
 
+////function declaration
 function print5(){
     console.log('inside print5', 5);
 }
+
+////function expression
 var print10 = function(){
     console.log('inside print10', 10);
 }
